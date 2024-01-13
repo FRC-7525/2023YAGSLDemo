a977127edb2bcb79a8b266acccaf130a7a9a8fc1
Shubh <shubhpatel2504@gmail.com>
2024-01-12 18:48:48 -0600

    Merged together IMU and Photonvision and began with navigation :)

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -1,12 +1,16 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
+package frc.robot.subsystems;
 
-package frc.robot;
+import java.util.Arrays;
+import java.util.Optional;
 
-import java.io.File;
-import java.io.IOException;
+import org.opencv.aruco.EstimateParameters;
+import org.photonvision.EstimatedRobotPose;
+import org.photonvision.PhotonCamera;
 
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
@@ -17,7 +21,6 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.subsystems.Drive;
-import frc.robot.subsystems.Vision;
 import swervelib.SwerveDrive;
 import swervelib.parser.SwerveParser;
 
@@ -55,85 +58,32 @@ import edu.wpi.first.wpilibj.interfaces.Accelerometer.Range;
 import edu.wpi.first.wpilibj.motorcontrol.MotorController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-/**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the
- * name of this class or
- * the package after creating this project, you must also update the
- * build.gradle file in the
- * project.
- */
-public class Robot extends TimedRobot {
-  public XboxController controller = new XboxController(0);
-  Drive drive = new Drive(this);
-  Vision vision = new Vision();
+public class Vision {
+    private Optional<EstimatedRobotPose> _optLastKnowPose;
+    PhotonCamera camera = new PhotonCamera("OV5647");
+  Transform3d robotToCam = new Transform3d(new Translation3d(0, 0.0, 0), new Rotation3d(0, 0, 0));
+  // AprilTagFieldLayout aprilTagFieldLayout = AprilTagFieldLayout
+  //     .loadFromResource(AprilTagFields.k2024Crescendo.m_resourceFile);
+  List<AprilTag> aprilTags = Arrays.asList(
+            new AprilTag(7, new Pose3d(0, 0, 0, new Rotation3d(0, 0, 0))),
+            new AprilTag(8, new Pose3d(0, 2, 0, new Rotation3d(0, 0, 0))));
+    AprilTagFieldLayout layout = new AprilTagFieldLayout(aprilTags, 3.0, 3.0);
+    PhotonPoseEstimator estimator = new PhotonPoseEstimator(layout, PoseStrategy.AVERAGE_BEST_TARGETS, camera, robotToCam);
+    final Pose3d targetPose = new Pose3d(0.0, 0.0, 0.0, new Rotation3d(0.0, 0.0, 0.0));
 
-  
-    
-    
-    
-    PIDController forwardController = new PIDController(0.9, 0, 0.0);
-    
-    final double ANGULAR_P = 0.025;
-    final double ANGULAR_D = 0.0;
-    PIDController angleController = new PIDController(ANGULAR_P, 0, ANGULAR_D);
 
-    SlewRateLimiter filter = new SlewRateLimiter(0.5);
-    double previous_distance;
-    
-
-  @Override
-  public void robotInit() {
-
-  }
-
-  @Override
-  public void robotPeriodic() {
-  }
-
-  @Override
-  public void autonomousInit() {
-  }
-
-  @Override
-  public void autonomousPeriodic() {
-  }
-
-  @Override
-  public void teleopInit() {
-  }
-  
-  @Override
-  public void teleopPeriodic() {
-  
-    if (vision.getPose2d().isPresent()) {
-      drive.addVisionMeasurement(vision.getPose2d(), Timer.getFPGATimestamp());
+    public void updateVision() {
+        Optional<EstimatedRobotPose> Botpose3d = estimator.update();
+        if (Botpose3d.isPresent()) {
+          _optLastKnowPose = Botpose3d;
+        }
     }
-    drive.periodic();
-  }
 
-  @Override
-  public void disabledInit() {
-  }
+    public Optional<Pose2d> getPose2d() {
+        if (_optLastKnowPose.isPresent()) {
+            return Optional.of(_optLastKnowPose.get().estimatedPose.toPose2d());
+        }
+        return Optional.empty();
+    }
 
-  @Override
-  public void disabledPeriodic() {
-  }
-
-  @Override
-  public void testInit() {
-  }
-
-  @Override
-  public void testPeriodic() {
-  }
-
-  @Override
-  public void simulationInit() {
-  }
-
-  @Override
-  public void simulationPeriodic() {
-  }
 }
